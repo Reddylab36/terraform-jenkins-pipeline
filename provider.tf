5510f765e09851348a81a2fd3dfa1feab89fe714
Reddylab36 <shekharreddy36@outlook.com>
2024-09-03 08:58:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "5.11.0"
     }
   }
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  # Configuration options
+  region = "us-east-1"
 }
