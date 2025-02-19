49910ac55d87eccaf94aa9548325b3f3a8f06203
MaheshGanji <msganji9998@gmail.com>
2025-02-19 21:02:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "pub-subnet3" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "198.162.0.0/16"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1"
   map_public_ip_on_launch = true
 }
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
-
+ security_groupd = ["default"]
  tags = {
-   Name = var.name_tag,
+   Name = var.name_tag
  }
 }
